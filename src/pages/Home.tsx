d591607b9ffd23054997edaf4cdbb25b3d6de99e
Mojeed Adeoye <mojeedeji@gmail.com>
2023-07-09 00:11:23 +0100

    mobile responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,9 +13,9 @@ const Home = () => {
       <Navbar />
 
       <div className='flex w-screen h-full'>
-        <section className='w-1/2 my-auto mx-12'>
-          <h1 className='text-6xl font-extrabold py-2 leading-tight'> Shrink, Track and Share Your Links </h1>
-          <p className='text-xl py-4 pr-24 font-light leading-snug'>
+        <section className='md:w-1/2 my-12 md:my-auto mx-6 md:mx-12'>
+          <h1 className='text-4xl md:text-6xl font-extrabold py-2 leading-tight'> Shrink, Track and Share Your Links </h1>
+          <p className='text-base md:text-xl py-4 md:pr-24 font-light leading-snug'>
             Shorten web links with our URL shortening services, so you can share the link more easily.
             We'll also create a QR code for each shortened link and provide analytics for your campaign.
           </p>
@@ -26,7 +26,7 @@ const Home = () => {
             </Link>
           </div>
         </section>
-        <section className='w-1/2 m-auto p-18 pr-24'>
+        <section className='w-1/2 m-auto p-18 pr-24 hidden md:block'>
           <img src={illustartion} alt="Illustration" />
         </section>
       </div>
